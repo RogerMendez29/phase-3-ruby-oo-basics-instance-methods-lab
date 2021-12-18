1f9a84fbaafd54e4e37d801900e0a08b008e691c
Roger Mendez <roger61087@gmail.com>
2021-12-18 10:54:10 -0500

    commit tree afce1a17f9a64bc80d20cd9c6ed5b0b923e4825f

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+require "pry"
+
+class Dog
+
+    def bark
+        puts "Woof!"
+        
+    end
+    
+
+    def sit
+        puts "The Dog is sitting"
+        
+    end
+
+    
+    
+    
+
+end
+
+
+fido = Dog.new
+
+
+
+# binding.pry
